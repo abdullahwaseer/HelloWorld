cf8dc39399e9cbfc7c50808394b2325fb8f65492
Bilal Ahmad <mega@Bilal-Ahmad-Ghouri.local>
2014-10-07 17:55:24 +0500

    first commit

---
+++ GitTesting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTesting
+//
+//  Created by Bilal Ahmad on 10/7/14.
+//  Copyright (c) 2014 Binex Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTesting/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GitTesting
+//
+//  Created by Bilal Ahmad on 10/7/14.
+//  Copyright (c) 2014 Binex Solutions. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
